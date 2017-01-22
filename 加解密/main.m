15c186a10358b0b77e75fee370b070abf44d8887
lotawei <128654907@qq.com>
2017-01-22 14:49:53 +0800

    Initial Commit

---
+++ 加解密/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  加解密
+//
+//  Created by lotawei on 17/1/22.
+//  Copyright © 2017年 lotawei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
